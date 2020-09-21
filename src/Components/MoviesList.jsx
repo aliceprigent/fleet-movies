8daed857a29f835df443b27fa8052673489140ef
Alice Prigent <aliprigent@gmail.com>
2020-09-21 12:46:41 +0200

    Initial commit

---
+++ src/Components/MoviesList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function MoviesList(props) {
+    console.log(props.movies.results)
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class SearchBar extends Component {
+    render() {
+        return (
+            <div>
+            <input type="text" name="search">
+                
+            </input>
+                
+            </div>
+        )
+    }
+}
+
+export default SearchBar
